abec298fbb7dae0379fd2e85a9bac9181a5f6c3c
Aatu Hieta <aatu@hieta.fi>
2018-05-14 15:53:06 +0300

    started working on a REPL for eddacmd

--- eddacmd/src/main.rs
+++ eddacmd/src/main.rs
@@ -1,23 +1,19 @@
 extern crate edda;
 
+use std::io::{self, BufRead};
+
 use edda::scanner::Scanner;
 use edda::parser::Parser;
 use edda::interpreter::Interpreter;
 
 fn main() {
-	let script = r#"
-print 1 + (3 * 4)*(3 * 4); // quick maths
-print "testi";
-
-let hello = "world";
-print hello;
-"#;
-
-	println!("Script: `{}`", script);
-
-    let tokens = Scanner::new(script.to_owned()).scan_tokens();
-    let statements = Parser::new(tokens).parse();
-
+    let mut EoF = false;
+    let stdin = io::stdin();
     let mut interpreter = Interpreter::new();
-    println!("Result: `{}`", interpreter.interpret(&statements).unwrap());
+
+    for line in stdin.lock().lines() {
+        let tokens = Scanner::new(line.unwrap()).scan_tokens();
+        let statements = Parser::new(tokens).parse();
+        interpreter.interpret(&statements).unwrap();
+    }
 }
